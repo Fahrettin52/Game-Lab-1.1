ad0dc4ada6c40ef501c49c4302e53b071ddebc07
ryuiji2 <superrief@hotmail.com>
2015-11-16 16:47:59 +0100

    Begin voor Throw Script.
    Rief

---
+++ Developing/The Garden Crusade/Assets/Scripts/ToThrow.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class ToThrow : MonoBehaviour {
+    public int rayDis;
+    public RaycastHit rayHit;
+    public bool canThrow;
+    public GameObject throwPrefab;
+    
+
+    void Start() {
+    }
+
+    void Update() {
+
+
+        if (canThrow == true && Input.GetButtonDown("G"))
+        {
+            Instantiate(throwPrefab, transform.position + new Vector3(0, 1.3f, 1), Quaternion.identity);
+            canThrow = false;
+        }
+        if (Input.GetButtonDown("F")) {
+            if (Physics.Raycast(transform.position + new Vector3(0, 1.3f, 0), transform.forward, out rayHit, rayDis))
+            {
+                if (rayHit.transform.tag == "mayThrow")
+                {
+                    Destroy(GameObject.FindWithTag("mayThrow"));
+                    canThrow = true;
+                }
+            }
+        }
+
+    }
+        void Throw () {
+        
+        }
+    }
