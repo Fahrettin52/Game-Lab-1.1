6a6d3c66310813dd373392a3aa2b601de0e9f307
svenevertse <svenevertse@gmail.com>
2015-11-20 08:48:21 +0100

    daily commit

---
+++ Developing/The Garden Crusade/Assets/Puzzle2.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Puzzle2 : MonoBehaviour {
+
+	public int checkStenen;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+	public void SteenCheck (){
+		if(checkStenen == 4){
+			print("Puzzle Complete");
+		}
+	}
+
+}
