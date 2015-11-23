bca2622a76e67fb5cb9eea6d6b1f60baeb1de0c8
Fahrettin52 <Fahrettin5252@hotmail.com>
2015-11-23 12:08:38 +0100

    Goed maken project

--- Developing/The Garden Crusade/Assets/Scripts/GameManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class GameManager : MonoBehaviour {
-
-    private static GameManager _instance;
-
-    public static GameManager Instance;
-}
