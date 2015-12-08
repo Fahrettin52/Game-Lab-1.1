077420de4bb08ebc1c6850b955a6604c911ec1f5
ryuiji2 <superrief@hotmail.com>
2015-12-08 22:53:45 +0100

    Test Ladybug

--- Developing/The Garden Crusade/Assets/Scripts/LadyBuggs.cs
+++ Developing/The Garden Crusade/Assets/Scripts/LadyBuggs.cs
@@ -4,8 +4,13 @@ using UnityEngine.UI;
 
 public class LadyBuggs : MonoBehaviour
 {
+    public Sprite ladybugImage;
 
     public enum Direction { One, Two, Three, Four }; 
     public Direction myDirection;
+
+    public void Start() {
+        GetComponent<Image>().sprite = ladybugImage;
+    }
 
 }
