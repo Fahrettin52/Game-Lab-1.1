afc3e287835b8625be75787397aa5056ebdd4924
ryuiji <superrief@hotmail.com>
2015-12-08 14:36:48 +0100

    die hele rechterSprite maar met code opgelost verdomme

--- Developing/The Garden Crusade/Assets/Scripts/LadyBuggs.cs
+++ Developing/The Garden Crusade/Assets/Scripts/LadyBuggs.cs
@@ -1,9 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class LadyBuggs : MonoBehaviour
 {
 
     public enum Direction { One, Two, Three, Four }; 
     public Direction myDirection;
+    public Sprite ladybugSlot1;
+
+
+    void Start()
+    {
+        GetComponent<Image>().sprite = ladybugSlot1;
+    }
 }
