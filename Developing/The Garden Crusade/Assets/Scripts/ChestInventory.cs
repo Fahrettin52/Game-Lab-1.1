2f1349d021809416b49490212ad272a589cecff2
Fahrettin52 <Fahrettin5252@hotmail.com>
2015-12-16 18:09:40 +0100

    Tutorial 9.2 Chestinventory stuff

---
+++ Developing/The Garden Crusade/Assets/Scripts/ChestInventory.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ChestInventory : Inventory {
+
+    private List<Stack<ItemScript>> chestItems;
+    private int chestSlots;
+    public int maxSlot;
+
+    public override void CreateLayout() {
+
+        allSlots = new List<GameObject>();
+
+        for (int i = 0; i < maxSlot; i++) {
+            GameObject newSlot = Instantiate(InventoryManager.Instance.slotPrefab);
+            newSlot.name = "Slot";
+            newSlot.transform.SetParent(this.transform);
+            allSlots.Add(newSlot);
+
+        }
+    }
+}
+ 
--- Developing/The Garden Crusade/Assets/Scripts/ChestScript.cs
+++ Developing/The Garden Crusade/Assets/Scripts/ChestScript.cs
@@ -1,7 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ChestScript : MonoBehaviour {
 
-	public Inventory chestInventory;
+	public ChestInventory chestInventory;
+
+    public int row, slots, slotSize;
+
+    private List<Stack<ItemScript>> allSlots;
+
+    void Start() {
+        allSlots = new List<Stack<ItemScript>>();
+    }
+
 }
+ 
