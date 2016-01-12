58a7ccf089387ac64f2df1558338abbd1a6454e7
svenevertse <svenevertse@gmail.com>
2016-01-12 17:00:07 +0100

    2d updated

--- Developing/The Garden Crusade 2.0/Assets/Scripts/TwoDimensionalMode.cs
+++ Developing/The Garden Crusade 2.0/Assets/Scripts/TwoDimensionalMode.cs
@@ -3,18 +3,33 @@ using System.Collections;
 
 public class TwoDimensionalMode : MonoBehaviour {
 
+	public GameObject mainCam;
+	public GameObject twoDCam;
+
 	void Start () {
+		mainCam = GameObject.Find("MainCamera");
+		twoDCam =  GameObject.Find("2DCamera"); 
+
 		if(Application.loadedLevel == 3){
 			GetComponent<Movement>().secondMode = true;
-			GameObject.Find("MainCamera").SetActive(false);
+			mainCam.SetActive(false);
 		}
 		else{
             GetComponent<Movement>().secondMode = false;
-            GameObject.Find("2DCamera").SetActive(false);
+            twoDCam.SetActive(false);
 		}
 	}
 	
 	void Update () {
 	
+	}
+
+	void OnTriggerEnter (Collider col) {
+		if(col.transform.name == "Switch"){
+			print("Switch");
+			GetComponent<Movement>().secondMode = false;
+			mainCam.SetActive(true);
+			twoDCam.SetActive(false);
+		}
 	}
 }
