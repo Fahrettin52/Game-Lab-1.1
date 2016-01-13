f0ba37248cd7ba3c2ca1f062d762ea442e44605e
Fahrettin52 <Fahrettin5252@hotmail.com>
2016-01-13 18:48:14 +0100

    Trow Gefixed met velocity

--- Developing/The Garden Crusade 2.0/Assets/Scripts/ToThrow.cs
+++ Developing/The Garden Crusade 2.0/Assets/Scripts/ToThrow.cs
@@ -19,7 +19,7 @@ public class ToThrow : MonoBehaviour {
     void Start() {
         throwInfo.SetActive(false);
         rb = GameObject.Find("Kruimels").GetComponent<Rigidbody>();
-        mayPickUp = 3f;
+        mayPickUp = 1f;
     }
 
     void Update() {
@@ -31,24 +31,21 @@ public class ToThrow : MonoBehaviour {
             if (Physics.Raycast(transform.position + new Vector3(0, 1.3f, 0), transform.forward, out rayHit, rayDis) ||
                (Physics.Raycast(transform.position + new Vector3(0, 0, 0), transform.forward, out rayHit, rayDis) ||
                (Physics.Raycast(transform.position + new Vector3(0, 2.6f, 0), transform.forward, out rayHit, rayDis)))) {
-                if (rayHit.transform.tag == "mayThrow" && mayPickUp == 0){
+                if (rayHit.transform.tag == "mayThrow" && mayPickUp == 0 && currentHold < 10){
                     currentHold++;
                     Destroy(rayHit.transform.gameObject);
                     throwInfo.SetActive(true);
-                    //canThrow = true;
                     throwInfo.GetComponent<Text>().text = changeText[1];
                     mayPickUp = 1;
                 }
             }
         if (Input.GetButtonDown("F") && currentHold <=maxHold && currentHold>0) {
             currentHold--;
-            Instantiate(throwPrefab, trowPos.transform.position + transform.forward, Quaternion.identity);
-            rb = throwPrefab.GetComponent<Rigidbody>();
-            rb.velocity = new Vector3(0, 0, 0) + transform.forward * throwSpeed;
+            GameObject tempPrefab = Instantiate(throwPrefab, trowPos.transform.position + transform.forward, Quaternion.identity) as GameObject;
+            tempPrefab.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0) + transform.forward * throwSpeed;
             if (currentHold == 0) {
                 throwInfo.SetActive(false);
             }
-            //canThrow = false;
             mayPickUp = 1;
         }
     }
