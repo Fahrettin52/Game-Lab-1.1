625cc624cd65926b431cd56998d548a7176bf90e
ryuiji2 <superrief@hotmail.com>
2016-01-27 22:33:43 +0100

    zwarte weduwe gedeeltelijk af, snap dingen niet

---
+++ Developing/The Garden Crusade 2.0/Assets/Scripts/HPZwarteWeduwe.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HPZwarteWeduwe : MonoBehaviour
+{
+
+    public Transform enemy;
+    public RectTransform bar;
+    public Transform player;
+    public Camera mainCamera;
+    public Text hpText;
+    public GameObject weduwe;
+
+    void Start() {
+        
+        
+    }
+
+    void Update()
+    {
+        mainCamera = GameObject.Find("MainCamera").GetComponent<Camera>();
+        player = GameObject.Find("Player").GetComponent<Transform>();
+        if (player != null)
+        {
+            bar.position = enemy.position + new Vector3(0, 2f, 0);
+            transform.rotation = Quaternion.LookRotation(transform.position - mainCamera.transform.position);
+            hpText.text = "Health: " + weduwe.GetComponent<ZwarteWeduwe>().livesEnemy.ToString("F0");
+        }
+    }
+}
---
+++ Developing/The Garden Crusade 2.0/Assets/Scripts/ZwarteWeduwe.cs
@@ -0,0 +1,133 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ZwarteWeduwe : MonoBehaviour
+{
+
+    private static ZwarteWeduwe instance;
+
+    public static ZwarteWeduwe Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<ZwarteWeduwe>();
+            }
+            return instance;
+        }
+    }
+
+    public Animator animator;
+    public Transform player;
+    public float distance;
+    public float range;
+    public float attackRange;
+    public float moveSpeed;
+    public int livesEnemy;
+    public GameObject dropRandomItem;
+    public Image fill;
+    public NavMeshAgent agent;
+    private bool resetBool;
+    public float navSpeed;
+    public bool idle;
+    public bool mayDie = false;
+    private bool mayDrop = true;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.Find("Player").transform;
+
+        if (idle == false)
+        {
+            navSpeed = GetComponent<NavMeshAgent>().speed;
+        }
+    }
+
+    void Update()
+    {
+        if (idle == false)
+        {
+            EnemyFollow();
+        }
+    }
+
+    void EnemyFollow()
+    {
+        if (player != null)
+        {
+            distance = Vector3.Distance(transform.position, player.position);
+            if (distance < 35)
+            {
+                //animator.SetBool("Threat", true);
+                agent.Stop();
+            }
+            else
+            {
+                //animator.SetBool("Threat", false);
+                agent.Resume();
+            }
+
+            if (distance < range)
+            {
+                FollowPlayer();
+            }
+            else
+            {
+                resetBool = true;
+            }
+
+            if (distance < attackRange)
+            {
+                //animator.SetTrigger("MayAttack");
+                agent.Stop();
+                transform.LookAt(player);
+            }
+            else
+            {
+                agent.Resume();
+            }
+        }
+    }
+
+    void FollowPlayer()
+    {
+
+        agent.SetDestination(player.transform.position);
+        if (resetBool == true)
+        {
+            transform.LookAt(agent.destination);
+            resetBool = false;
+        }
+    }
+
+    public void DropDead(int damage)
+    {
+        livesEnemy -= damage;
+        fill.fillAmount -= (damage / 100f);
+        if (livesEnemy < 1)
+        {
+            livesEnemy = 0;
+        }
+        if (livesEnemy == 0)
+        {
+            mayDie = true;
+        }
+        if (livesEnemy == 0 && mayDie == true && mayDrop == true)
+        {
+            mayDie = false;
+            GameObject.Find("_Manager").GetComponent<ToSceneOne>().general = null;
+            GameObject.Find("Player").GetComponent<Quests>().currentObjective += 1;
+            GameObject.Find("Player").GetComponent<Quests>().currentObjectiveText += 1;
+            GameObject.Find("Player").GetComponent<Quests>().LoopForBool();
+            GameObject.Find("Player").GetComponent<Experience>().currentExp += GameObject.Find("Player").GetComponent<Experience>().expGet;
+            //animator.SetTrigger("MayDie");
+            Destroy(gameObject, 1f);
+            GameObject.Instantiate(dropRandomItem).transform.position = transform.position;
+            mayDrop = false;
+        }
+    }
+}
+
